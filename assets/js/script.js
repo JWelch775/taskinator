ebe052cd0ea030999e19711875219b5fb26891c6
Justin Welch <justin.welch075@gmail.com>
2021-01-11 12:57:30 -0600

    added function to create new li for task list

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+//function for adding list items to the task section
+var createTaskHandler = function()
+    {
+        var listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.textContent = "This is a new task.";
+        tasksToDoEl.appendChild(listItemEl);
+    };
+    
+    
+    
+    
+    
+    
+    
+    
+    //function call for creating list items in task section
+    buttonEl.addEventListener("click", createTaskHandler);
