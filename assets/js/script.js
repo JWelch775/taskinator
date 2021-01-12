74d5c2c10e831039412116a0299d538c6867eb95
Justin Welch <justin.welch075@gmail.com>
2021-01-12 13:21:00 -0600

    split existing function ito taskFormHander and createTaskEl functions for better performance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,45 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//function for adding list items to the task section
-var createTaskHandler = function(event)
+//function for collecting data from the task-form
+var taskFormHandler = function(event)
     {
         event.preventDefault();
 
-        var listItemEl = document.createElement("li");
-        listItemEl.className = "task-item";
-        listItemEl.textContent = "This is a new task.";
-        tasksToDoEl.appendChild(listItemEl);
-    };
+        var taskNameInput = document.querySelector("input[name= 'task-name']").value;
+        var taskTypeInput = document.querySelector("select[name= 'task-type']").value;
+
+        //package up data as an object
+        var taskDataObj = 
+            {
+                name: taskNameInput,
+                type: taskTypeInput
+            };
+
+        //send it as an argument to createTaskEl
+        creatTaskEl(taskDataObj);
+        
+        
+    }
     
-    
+    //function for turning task-form data into printable html elements
+    var creatTaskEl = function(taskDataObj)
+        {
+            //create list item
+            var listItemEl = document.createElement("li");
+            listItemEl.className = "task-item";
+
+            //create div to hold task info and add to list item
+            var taskInfoEl = document.createElement("div");
+            taskInfoEl.className = "task-info";
+
+            //add html content to div
+            taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+            listItemEl.appendChild(taskInfoEl);
+
+            //add entire list item to list
+            tasksToDoEl.appendChild(listItemEl);
+        }
     
     
     
@@ -20,4 +47,4 @@ var createTaskHandler = function(event)
     
     
     //function call for creating list items in task section from #task-form
-    formEl.addEventListener("submit", createTaskHandler);
+    formEl.addEventListener("submit", taskFormHandler);
